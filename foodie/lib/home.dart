65c5b84f8498ddcb7b81e70bb0fbdbcbd4da9c71
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-06 11:09:49 +0545

    Search Bar
    Search bar added.

--- foodie/lib/home.dart
+++ foodie/lib/home.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Container(
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               gradient: LinearGradient(
                 colors: [
                   Color(0xff213A50),
@@ -24,6 +25,52 @@ class _HomeScreenState extends State<HomeScreen> {
                 ],
               ),
             ),
+          ),
+          Column(
+            children: [
+              //serach bar
+              SafeArea(
+                  child: Container(
+                    //Search Container
+
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    margin:  const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24)),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if ((searchController.text).replaceAll(" ", "") ==
+                                "") {
+                              print("Blank search");
+                            } else {
+                             //Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                            }
+                          },
+                          child: Container(
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            margin:const EdgeInsets.fromLTRB(3, 0, 7, 0),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextField(
+                            controller: searchController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Let's Cook Something!"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+              ),
+
+            ],
           ),
           SafeArea(
             child: Text(
