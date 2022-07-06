81a826cc8af5806e82a0c12626bf7bec575a1472
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-06 10:23:03 +0545

    Update home.dart
    Added Stack and Linear Gradient color. Responsive height and width with is added using Media Query.

--- foodie/lib/home.dart
+++ foodie/lib/home.dart
@@ -10,6 +10,29 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff213A50),
+                  Color(0xff071938),
+                ],
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Text(
+              'Hi I am Home Screen',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
