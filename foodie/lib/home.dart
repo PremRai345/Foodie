468d22e44c86e6b4034c14cc4b710ac791a62324
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-08 11:31:39 +0545

    List view
    List view is added.

--- foodie/lib/home.dart
+++ foodie/lib/home.dart
@@ -59,67 +59,82 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          Column(
-            children: [
-              SafeArea(
-                child: Container(
-                  //Search Wala Container
-
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24)),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          if ((searchController.text).replaceAll(" ", "") ==
-                              "") {
-                            print("Blank search");
-                          } else {
-                            getRecipe(searchController.text);
-                          }
-                        },
-                        child: Container(
-                          child: Icon(
-                            Icons.search,
-                            color: Colors.blueAccent,
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SafeArea(
+                  child: Container(
+                    //Search Wala Container
+          
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24)),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if ((searchController.text).replaceAll(" ", "") ==
+                                "") {
+                              print("Blank search");
+                            } else {
+                              getRecipe(searchController.text);
+                            }
+                          },
+                          child: Container(
+                            child: Icon(
+                              Icons.search,
+                              color: Colors.blueAccent,
+                            ),
+                            margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                           ),
-                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                         ),
+                        Expanded(
+                          child: TextField(
+                            controller: searchController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Lets Cook Somethings"),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: const [
+                      Text(
+                        "What do you want to cook?",
+                        style: TextStyle(fontSize: 33, color: Colors.white),
                       ),
-                      Expanded(
-                        child: TextField(
-                          controller: searchController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Lets Cook Somethings"),
-                        ),
-                      )
+                      SizedBox(height: 10),
+                      Text("Lets Cook!",
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
                     ],
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: const [
-                    Text(
-                      "What do you want to cook?",
-                      style: TextStyle(fontSize: 33, color: Colors.white),
-                    ),
-                    SizedBox(height: 10),
-                    Text("Lets Cook!",
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
-                  ],
+                Container(
+                  child: ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return PremText();
+                      }),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+Widget PremText() {
+  return const Text("Prem is a Flutter developer");
 }
