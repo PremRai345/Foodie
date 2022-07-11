0738ef90a9bd44675c4c27976a291a67773cdad3
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-11 18:36:17 +0545

    InkWell, Card, Positioned
    Added Inwell Card and positioned to display data from api.

--- foodie/lib/home.dart
+++ foodie/lib/home.dart
@@ -65,10 +65,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 SafeArea(
                   child: Container(
                     //Search Wala Container
-          
+
                     padding: const EdgeInsets.symmetric(horizontal: 8),
-                    margin:
-                        const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 24, vertical: 20),
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(24)),
@@ -119,11 +119,83 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 Container(
                   child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
+                      physics: const NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
-                      itemCount: 100,
+                      itemCount: recipeList.length,
                       itemBuilder: (context, index) {
-                        return PremText();
+                        return InkWell(
+                          onTap: () {},
+                          child: Card(
+                            margin: const EdgeInsets.all(20),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            elevation: 0.0,
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: Image.network(
+                                    recipeList[index].appimgUrl,
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                    height: 200,
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 0,
+                                  right: 0,
+                                  bottom: 0,
+                                  child: Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 10),
+                                    decoration: const BoxDecoration(
+                                      color: Colors.black26,
+                                    ),
+                                    child: Text(
+                                      recipeList[index].applabel,
+                                      style: const TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  right: 0,
+                                  width: 80,
+                                  height: 42,
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.only(
+                                        topRight: Radius.circular(10),
+                                        bottomLeft: Radius.circular(10),
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          const Icon(
+                                            Icons.local_fire_department,
+                                            color: Colors.red,
+                                            size: 16,
+                                          ),
+                                          Text(
+                                            recipeList[index]
+                                                .appcalories
+                                                .toString()
+                                                .substring(0, 6),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
                       }),
                 ),
               ],
